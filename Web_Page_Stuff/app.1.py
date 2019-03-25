83683665966e8ffc13e23f9efcf7353d9f44f600
jfandata <13265336+jfandata@users.noreply.github.com>
2019-03-25 17:47:29 -0500

    Add files via upload

--- Web_Page_Stuff/app.1.py
+++ Web_Page_Stuff/app.1.py
@@ -6,7 +6,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-
+from urllib.request import urlopen
 import string
 import datetime
 
@@ -26,10 +26,35 @@ from keras.applications.xception import (
     Xception, preprocess_input, decode_predictions)
 from keras import backend as K
 
+from google.cloud import translate
+target = 'es'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Languagelearningapp-07a1e1d6b374.json"
+translate_client = translate.Client()
+
 from flask import Flask, request, redirect, url_for, jsonify, render_template
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
+
+model = None
+graph = None
+
+
+def load_model():
+    global model
+    global graph
+    model = Xception(weights="imagenet")
+    graph = K.get_session().graph
+
+load_model()
+
+def prepare_image(img):
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    # return the processed image
+    return img
+
 
 @app.route("/")
 def index():
@@ -50,10 +75,9 @@ def index():
 def upload_file():
     data = {"success": False}
     if request.method == 'POST':
-        if request.files.get('file'):
-            print("file if")
+        if request.files.get('img_file'):
             # read the file
-            file = request.files['file']
+            file = request.files['img_file']
 
             # read the filename
             filename = file.filename
@@ -62,76 +86,95 @@ def upload_file():
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             
             now = datetime.datetime.now()
-            cd = cloudinary.uploader.upload(file,folder = "rudsfinal")
-            cdFilename= cd['secure_url']
-            send.append({"filepath": cdFilename})
+            cd = cloudinary.uploader.upload(file, folder = "rudsfinal")
+            filepath= cd['url']
+            image_size = (299, 299)
+            im = ''
+
+            with urlopen(filepath) as img:
+                im = keras.preprocessing.image.load_img(img,
+                                                    target_size=image_size,
+                                                    grayscale=False)
+            # preprocess the image and prepare it for classification
+            image = prepare_image(im)
             
-            file.save(filepath)
-        return jsonify({"test":"test"})
+            global graph
+            with graph.as_default():
+                preds = model.predict(image)
+                results = decode_predictions(preds)
+                # print the results
+                print(results)
+
+                data["predictions"] = []
+
+                # loop over the results and add them to the list of
+                # returned predictions
+                for (imagenetID, label, prob) in results[0]:
+                    
+    
+                    translation = translate_client.translate(label, target_language=target)
+  
+                    r = {"label": label, "translation": translation["translatedText"]}
+                    data["predictions"].append(r)
 
 
-@app.route('/richard/<image>', methods=['GET', 'POST'])
-def richard(image):
-    img_file = f"./uploads/{image}"
-    test = cloudinary.uploader.upload(img_file)
-    return jsonify(test)
+                # indicate that the request was a success
+                data["success"] = True
+        return jsonify(data)
+
+# @app.route("/getfile", methods=['GET', 'POST'])
+# def getFile():
 
 
-
-@app.route("/getfile", methods=['GET', 'POST'])
-def getFile():
-
-
-    model = None
-    graph = None
+#     model = None
+#     graph = None
 
 
-def load_model():
-    global model
-    global graph
-    model = Xception(weights="imagenet")
-    graph = K.get_session().graph
+# def load_model():
+#     global model
+#     global graph
+#     model = Xception(weights="imagenet")
+#     graph = K.get_session().graph
 
 
-load_model()    
+# load_model()
 
 
-def prepare_image(img):
-    img = img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)
-    # return the processed image
-    return img
+#     # Load the saved image using Keras and resize it to the Xception
+#     # format of 299x299 pixels
+#     image_size = (299, 299)
+#     im = keras.preprocessing.image.load_img(filepath,
+#                                             target_size=image_size,
+#                                             grayscale=False)
 
-    # Load the saved image using Keras and resize it to the Xception
-    # format of 299x299 pixels
-    image_size = (299, 299)
-    im = keras.preprocessing.image.load_img(filepath,
-                                                target_size=image_size,
-                                                grayscale=False)
+#     # preprocess the image and prepare it for classification
+#     image = prepare_image(im)
 
-        # preprocess the image and prepare it for classification
-    image = prepare_image(im)
+#     global graph
+#     with graph.as_default():
+#         preds = model.predict(image)
+#         results = decode_predictions(preds)
+#         # print the results
+#         print(results)
 
-    global graph
-    with graph.as_default():
-            preds = model.predict(image)
-            results = decode_predictions(preds)
-            # print the results
-            print(results)
+#         data["predictions"] = []
 
-            data["predictions"] = []
+#         # loop over the results and add them to the list of
+#         # returned predictions
+#         for (imagenetID, label, prob) in results[0]:
+            
 
-            # loop over the results and add them to the list of
-            # returned predictions
-            for (imagenetID, label, prob) in results[0]:
-                r = {"label": label, "probability": float(prob)}
-                data["predictions"].append(r)
+#             translation = translate_client.translate(label, target_language=target)
 
-            # indicate that the request was a success
-            data["success"] = True
+#             r = {"label": label, "translation": translation["translatedText"]}
+#             data["predictions"].append(r)
 
-    return jsonify(data)
+
+#         # indicate that the request was a success
+#         data["success"] = True
+
+
+#     return jsonify(data)
 
     
 
